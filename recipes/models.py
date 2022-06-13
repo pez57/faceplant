dfd8cbd8abb3839695e849ebbbbbcd705753f7a5
pez57 <perry_s@live.com>
2022-06-13 09:22:04 +0000

    Added homepage layout and fixed image sizing in cards

--- recipes/models.py
+++ recipes/models.py
@@ -23,7 +23,7 @@ class Recipe(models.Model):
     likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
@@ -32,9 +32,11 @@ class Recipe(models.Model):
         return self.likes.count()
 
 class Comment(models.Model):
-    recipe = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
+    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
